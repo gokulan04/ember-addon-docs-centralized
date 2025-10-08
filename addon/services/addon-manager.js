fe92f3a9490e218fe2227eabad426211e3e8704c
GokulanGM <gokulan@admindroid.com>
2025-10-09 01:53:32 +0530

    current project updated for route change and initial load.

--- addon/services/addon-manager.js
+++ addon/services/addon-manager.js
@@ -1,14 +1,17 @@
 import Service from '@ember/service';
+import { service } from '@ember/service';
 import { tracked } from '@glimmer/tracking';
 import { addonDocsConfig } from 'ember-cli-addon-docs/-private/config';
 
 export default class AddonManagerService extends Service {
     @addonDocsConfig config; 
+    @service router;
     @tracked currentProjectName = "";
     
     constructor() {
         super(...arguments);
-        this.setCurrentProject(this.config.projects[Object.keys(this.config.projects)[0]].projectName);
+        this.setCurrentProject(this.router.currentRoute.parent.name);
+        this.router.on('routeDidChange', (transition) => this.setCurrentProject(transition.to.parent.name));
     }
     
     get currentProject(){
