2a33b83305a9b64aaff3de2adc100eacadcc28e2
GokulanGM <gokulan@admindroid.com>
2025-10-22 19:40:58 +0530

    doc routes removed , } closed properly

--- tests/dummy/app/router.js
+++ tests/dummy/app/router.js
@@ -10,25 +10,6 @@ export default class Router extends AddonDocsRouter {
 }
 
 Router.map(function () {
-  docsRoute(this, function () {
-    this.route('usage');
-    this.route('quickstart');
-    this.route('patterns');
-    this.route('build-options');
-    this.route('deploying');
-    this.route('standalone-apps');
-    this.route('upgrade-to-5');
-
-    this.route('components', function () {
-      this.route('docs-hero');
-      this.route('docs-logo');
-      this.route('docs-header');
-      this.route('docs-snippet');
-      this.route('docs-viewer');
-      this.route('docs-demo');
-    });
-  });
-
   this.route('sandbox', function () {
     apiRoute(this);
     docsRoute(this, function () {
